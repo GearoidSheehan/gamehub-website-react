6a641ff7cc84c06bbfb644fd517190ad1f582f7c
Gearóid Sheehan <33189671+GearoidSheehan@users.noreply.github.com>
2023-04-05 17:27:18 +0100

    Create a custom hook to fetch the games.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,32 +1,9 @@
 import { Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-//Interface for an individual game object returned from the rawg.io API
-interface Game {
-  id: number;
-  name: string;
-}
-
-//Interface for the array of game objects returned from the rawg.io API
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import { useGames } from "../hooks/useGames";
 
 const GameGrid = () => {
-  //Saving the state of the games returned from the rawg.io API and/or the errors
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  //API call to the rawg.io API to get the array of games, with error handling
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
-
+  //Hook to make rawg.io API call
+  const { games, error } = useGames();
   return (
     //If error occurs in rawg.io API call then show error text. Otherwise show return from API as a list
     <>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,46 @@
+import { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+//Interface for an individual game object returned from the rawg.io API
+interface Game {
+  id: number;
+  name: string;
+}
+
+//Interface for the array of game objects returned from the rawg.io API
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+export const useGames = () => {
+  //Saving the state of the games returned from the rawg.io API and/or the errors
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  //API call to the rawg.io API to get the array of games, with error handling
+  useEffect(() => {
+    /*
+      Uses the browsers AbortController API to handle cancellations. When called this causes 
+      the fetch operation and every subsequent, then() method to be discarded, and the catch() 
+      method to execute. The catch() block then has a check to differentiate whether an actual 
+      error occurred, which would require an error message to be set in the state.
+    */
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+
+    //The array below is an array of dependencies. This prevents constant calls to the backend.
+  }, []);
+
+  return { games, error };
+};
