fd92b2256b99e8723ec8e51ef3fe8d0347c6191b
Gearóid Sheehan <33189671+GearoidSheehan@users.noreply.github.com>
2023-04-14 10:55:01 +0100

    Build search input.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    //HStack aligns items horizontally. Row is also an alias for HStack. Positioned at the right hand side of screen with padding.
-    <HStack justifyContent="space-between" padding="10px">
+    //HStack aligns items horizontally. Row is also an alias for HStack.
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
