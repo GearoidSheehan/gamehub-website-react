2ee65de84e62055aebdf602d04fa15c8b8298ce7
Gearóid Sheehan <33189671+GearoidSheehan@users.noreply.github.com>
2023-04-20 12:19:21 +0100

    Refactoring GameGrid.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,31 +15,31 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
+  /*
+    If error occurs in rawg.io API call then show error text. Otherwise show 
+    return from API as a list. Show skeletons if isLoading state is true.
+  */
+
+  if (error) return <Text>{error}</Text>;
+
   return (
-    /*
-      If error occurs in rawg.io API call then show error text. Otherwise show 
-      return from API as a list. Show skeletons if isLoading state is true.
-    */
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />{" "}
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />{" "}
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
